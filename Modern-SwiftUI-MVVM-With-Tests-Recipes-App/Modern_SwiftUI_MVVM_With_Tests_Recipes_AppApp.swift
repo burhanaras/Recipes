98508437922e2e832220774de83e8944c150c90b
Burhan Aras <burhan.aras@paydustry.com>
2021-09-06 13:49:05 +0300

    Initial Commit

---
+++ Modern-SwiftUI-MVVM-With-Tests-Recipes-App/Modern_SwiftUI_MVVM_With_Tests_Recipes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modern_SwiftUI_MVVM_With_Tests_Recipes_AppApp.swift
+//  Modern-SwiftUI-MVVM-With-Tests-Recipes-App
+//
+//  Created by Burhan Aras on 6.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Modern_SwiftUI_MVVM_With_Tests_Recipes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
